c857bd60c8b42c2e9e8abe3ac39fac5202effbd8
haytaylan <hakan.aytaylan@gmail.com>
2021-07-11 10:31:13 +0300

    Chapter 13

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import ReactDOM from 'react-dom';
 import GlobalStyle from '/components/GlobalStyle';
 // import routes
 import Pages from '/pages';
+// import Apollo Client libraries
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+// configure Apollo Client
+const client = new ApolloClient({
+    uri,
+    cache,
+    connectToDevTools: true
+});
 
 const App = () => {
     return (
-        <div>
+        <ApolloProvider client={client}>
             <GlobalStyle />
             <Pages />
-        </div>
+        </ApolloProvider>
     );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,17 @@ import Layout from '../components/Layout';
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
+import NotePage from './note';
 
 // define routes
 const Pages = () => {
     return (
         <Router>
-            {/* Wrap our routes within the Layout component */}
             <Layout>
                 <Route exact path="/" component={Home} />
                 <Route path="/mynotes" component={MyNotes} />
                 <Route path="/favorites" component={Favorites} />
+                <Route path="/note/:id" component={NotePage} />
             </Layout>
         </Router>
     );
