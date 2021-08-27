268246f83cf2d7190adba1235a4f47d88c97830c
haytaylan <hakan.aytaylan@gmail.com>
2021-08-28 00:35:21 +0300

    Chapter 15

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 // import React and routing dependencies
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
 
 // import shared layout component
 import Layout from '../components/Layout';
@@ -10,6 +11,14 @@ import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
 import NotePage from './note';
+import SignUp from './signup';
+import SignIn from './signin';
+
+const IS_LOGGED_IN = gql`
+  {
+    isLoggedIn @client
+  }
+`;
 
 // define routes
 const Pages = () => {
@@ -17,11 +26,40 @@ const Pages = () => {
         <Router>
             <Layout>
                 <Route exact path="/" component={Home} />
-                <Route path="/mynotes" component={MyNotes} />
-                <Route path="/favorites" component={Favorites} />
-                <Route path="/note/:id" component={NotePage} />
+                <PrivateRoute path="/mynotes" component={MyNotes} />
+                <PrivateRoute path="/favorites" component={Favorites} />
+                <Route path="/note/:id" component={Note} />
+                <Route path="/signup" component={SignUp} />
+                <Route path="/signin" component={SignIn} />
             </Layout>
         </Router>
+    );
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const { loading, error, data } = useQuery(IS_LOGGED_IN);
+    // if the data is loading, display a loading message
+    if (loading) return <p>Loading...</p>;
+    // if there is an error fetching the data, display an error message
+    if (error) return <p>Error!</p>;
+    // if the user is logged in, route them to the requested component
+    // else redirect them to the sign-in page
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                data.isLoggedIn === true ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: '/signin',
+                            state: { from: props.location }
+                        }}
+                    />
+                )
+            }
+        />
     );
 };
 
